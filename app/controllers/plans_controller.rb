94e7e391700d41dbd9e9e43354dcd2f294404464
Btakizawa18 <akiduki.1329@gmail.com>
2023-12-26 22:27:04 +0900

    バリデーション、コントローラー、ルーティングの設定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,16 @@
 class PlansController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:image, :title, :detail, :goal, :start_time).merge(user_id: current_user.id)
+  end
 end
